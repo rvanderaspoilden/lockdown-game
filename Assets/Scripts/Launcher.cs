a3f4742fd6aaf7743f431cc3d4da66253c03633b
rvanderaspoilden <rodolphevanderaspoilden@gmail.com>
2020-03-28 15:04:04 +0100

    :construction: wip

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -1,4 +1,5 @@
-﻿using Photon.Pun;
+﻿using System;
+using Photon.Pun;
 using Photon.Realtime;
 using TMPro;
 using UnityEngine;
@@ -7,20 +8,32 @@ using Utils;
 public class Launcher : MonoBehaviourPunCallbacks {
     [Header("Settings")]
     [SerializeField] private byte maxPlayers;
-    
-    [Header("UI fields")]
+
+    [Header("UI fields for authentification")]
+    [SerializeField] private GameObject authentificationPanel;
     [SerializeField] private TMP_InputField usernameInputField;
     [SerializeField] private TMP_InputField roomIdInputField;
+
+    [Header("UI fields for room")]
+    [SerializeField] private GameObject roomPanel;
+    [SerializeField] private TextMeshProUGUI roomIdText;
     
     [Header("Only for debug")]
     [SerializeField] private string username;
     [SerializeField] private string roomId;
 
+    private void Awake() {
+        this.DisplayAuthentificationPanel();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        // Listener on inputs
         this.usernameInputField.onValueChanged.AddListener((string value) => this.SetUsername(value));
         this.roomIdInputField.onValueChanged.AddListener((string value) => this.SetRoomId(value));
+        
+        // Photon connection
         PhotonNetwork.AutomaticallySyncScene = true;
         this.Connect();
     }
@@ -77,6 +90,20 @@ public class Launcher : MonoBehaviourPunCallbacks {
         PhotonNetwork.JoinRoom(this.roomId);
     }
 
+    public void LeaveRoom() {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    private void DisplayAuthentificationPanel() {
+        this.authentificationPanel.SetActive(true);
+        this.roomPanel.SetActive(false);
+    }
+    
+    private void DisplayRoomPanel() {
+        this.authentificationPanel.SetActive(false);
+        this.roomPanel.SetActive(true);
+    }
+
     #region ConnectionCallbacks
 
     public override void OnConnectedToMaster() {
@@ -102,11 +129,16 @@ public class Launcher : MonoBehaviourPunCallbacks {
 
     public override void OnJoinRoomFailed(short returnCode, string message) {
         Debug.LogErrorFormat("Join room with id {0} failed cause {1}", this.roomId, message);
-
     }
 
     public override void OnJoinedRoom() {
         Debug.Log("Room joined with id : " + PhotonNetwork.CurrentRoom.Name);
+        
+        this.DisplayRoomPanel();
+    }
+
+    public override void OnLeftRoom() {
+        this.DisplayAuthentificationPanel();
     }
 
     #endregion
