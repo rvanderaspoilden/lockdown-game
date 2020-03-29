913eccc51cc4edc97a4ac725c477edcd0dfc895f
rvanderaspoilden <rodolphevanderaspoilden@gmail.com>
2020-03-29 16:14:43 +0100

    :construction: covid wip

--- Assets/Scripts/Game/Player/PlayerInteraction.cs
+++ Assets/Scripts/Game/Player/PlayerInteraction.cs
@@ -8,13 +8,41 @@ namespace Game.Player {
         [Header("Only for debug")]
         [SerializeField] private PlayerHands hands;
 
+        [SerializeField] private PlayerEntity playerEntity;
+
+        private Coroutine covidCoroutine;
+
         private void Awake() {
+            this.playerEntity = GetComponent<PlayerEntity>();
             this.hands = GetComponent<PlayerHands>();
         }
 
         private void OnTriggerEnter(Collider other) {
             if (other.CompareTag("Weapon") && !this.hands.HasWeapon()) {
                 this.hands.SetWeapon(other.GetComponent<Weapon>());
+            }
+        }
+
+        private void OnTriggerStay(Collider other) {
+            if (other.CompareTag("CovidArea") && covidCoroutine == null) {
+                Debug.Log("Enter in covid area");
+                this.covidCoroutine = StartCoroutine(this.TakeDamageFromCovid());
+            }
+        }
+
+        private void OnTriggerExit(Collider other) {
+            if (other.CompareTag("CovidArea")) {
+                Debug.Log("Covid left");
+                StopCoroutine(this.covidCoroutine);
+                this.covidCoroutine = null;
+            }
+        }
+
+        private IEnumerator TakeDamageFromCovid() {
+            while (true) {
+                yield return new WaitForSeconds(3f);
+                this.playerEntity.TakeDamage(10f);
+                Debug.Log("Take damage from covid");
             }
         }
     }
